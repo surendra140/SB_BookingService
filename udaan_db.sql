f41000d6d115d0695e081b195da72919df5fd1f7
Surendra Bankar <60913523+surendra140@users.noreply.github.com>
2025-06-04 21:21:02 +0530

    db schema file

---
+++ udaan_db.sql
@@ -0,0 +1,8 @@
+/*This file for database related operations on udaan application*/
+create database flightDB;
+create database bookingDB;
+create database routeDB;
+create database scheduleDB;
+create database securityDB;
+create database feedbackDB;
+
